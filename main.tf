f2c93fe5bc9331c2f358ccb9fd9559366bd65c2c
kailash1987vinod-droid <kailash1987vinod@gmail.com>
2025-12-02 22:20:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,78 +1,122 @@
 terraform {
-  required_version = ">= 1.6.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  # Credentials are pulled from your PowerShell environment variables automatically
 }
 
-# 1) Resource Group
+# 1. Create Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-ubuntu-vm"
-  location = "Central India"
+  name     = var.resource_group_name
+  location = var.location
 }
 
-# 2) Virtual Network
+# 2. Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "ubuntu-vnet"
+  name                = "yash-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# 3) Subnet
+# 3. Create Subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "ubuntu-subnet"
+  name                 = "internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# 4) Public IP
+# 4. Create Public IP
 resource "azurerm_public_ip" "public_ip" {
-  name                = "ubuntu-public-ip"
-  location            = azurerm_resource_group.rg.location
+  name                = "yash-public-ip"
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
 }
 
-# 5) Network Interface
+# 5. Create Network Security Group (Firewall)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "yash-firewall-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# 6. Create Network Interface
 resource "azurerm_network_interface" "nic" {
-  name                = "ubuntu-nic"
+  name                = "yash-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "nic-ip"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
-# 6) Ubuntu VM
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "ubuntu-vm"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
+# 7. Connect NSG to NIC
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = var.ssh_public_key
-  }
+# 8. Create Ubuntu Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = "yash-ubuntu-vm"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_B1s" # Economical size
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
 
   network_interface_ids = [
-    azurerm_network_interface.nic.id
+    azurerm_network_interface.nic.id,
   ]
 
   os_disk {
@@ -88,13 +132,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 }
 
-# ---- VARIABLES & OUTPUTS ----
-
-variable "ssh_public_key" {
-  description = "SSH public key for the VM"
-  type        = string
-}
-
-output "public_ip" {
-  value = azurerm_public_ip.public_ip.ip_address
+# Output the Public IP so you know where to connect
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.vm.public_ip_address
 }
