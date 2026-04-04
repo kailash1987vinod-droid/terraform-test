e39c22e500eb336f71002872022849a8ad346c1a
kailash1987vinod-droid <kailash1987vinod@gmail.com>
2026-04-05 02:19:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region"
   type        = string
-  default     = "East US"
+  default     = "eastus2"
 }
 
 variable "admin_username" {
